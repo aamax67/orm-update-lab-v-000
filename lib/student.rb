e693a686cff17176a8b5de4eed0eb041447f538f
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-03-29 19:16:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,9 +23,7 @@ attr_reader :id
   end
 
   def self.drop_table
-  sql = <<-SQL
-    DROP TABLE students
-  SQL
+    sql = "DROP TABLE IF EXISTS students"
   DB[:conn].execute(sql)
   end
 
@@ -43,10 +41,23 @@ attr_reader :id
     end
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
 
   def update
